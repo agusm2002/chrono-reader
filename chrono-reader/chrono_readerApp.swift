278725b90d8c761dd94f7b20f9b96cee2901072d
Agustín Monti <agustinmonti2002@gmail.com>
2025-03-03 21:47:54 -0300

    Initial Commit

---
+++ chrono-reader/chrono_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  chrono_readerApp.swift
+//  chrono-reader
+//
+//  Created by Agustin Monti on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct chrono_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
